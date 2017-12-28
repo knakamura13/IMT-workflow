1775904e538ab675e32a2bed0014524c1eeb779e
Kyle Nakamura <knakamura13@apu.edu>
2017-12-28 13:04:54 -0700

    Initial collection view setup complete

---
+++ IMT-workflow/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  IMT-workflow
+//
+//  Created by Kyle Nakamura on 12/28/17.
+//  Copyright © 2017 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+}
